26528a4aa7abca6199bbce23f0a31b42ed07cc40
Nick <nick.heskes@scenius.nl>
2019-09-11 11:48:02 +0200

    Also export relevant api's

--- projects/clr-dialog/src/public-api.ts
+++ projects/clr-dialog/src/public-api.ts
@@ -3,3 +3,9 @@
  */
 
 export * from './lib/clr-dialog.module';
+export * from './lib/clr-dialog-animations';
+export * from './lib/clr-dialog-config';
+export * from './lib/clr-dialog-container';
+export * from './lib/clr-dialog-content-directives';
+export * from './lib/clr-dialog-ref';
+export * from './lib/clr-dialog.service';
